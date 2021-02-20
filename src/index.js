692bdb1483bdbc568c6436a013de9c416166550e
GEORGE BURTON <georgeburton@live.co.uk>
2021-02-20 19:19:44 +0000

    created tabs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './ui/tooltip'
 import Dropdown from './ui/dropdown'
+import Tabs from './ui/tabs'
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -13,3 +14,7 @@ dropdowns.forEach(dropdown => {
     const instance = new Dropdown(dropdown);
     instance.init();
 })
+
+// Create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
