58cc22252d400a701ad4e444bab3678273a77c99
GEORGE BURTON <georgeburton@live.co.uk>
2021-02-20 17:03:37 +0000

    Added dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Tooltip from './ui/tooltip'
+import Dropdown from './ui/dropdown'
 
 // create a tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
 tooltip.init()
+
+// Create Dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
